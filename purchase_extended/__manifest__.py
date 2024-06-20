447e21746b01484a134d6d8a16b4cf152b74a8a2
riccino <riccardorinojosam@gmail.com>
2024-06-20 10:23:15 -0300

    Create new module purchase_extended Add __manifest__.py Create PurchaseOrder class inheriting from purchase.order in models folder

---
+++ purchase_extended/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Purchase Extended",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Riccino",
+    'category': 'Category',
+    'description': """
+    An purchase extension module
+    """,
+
+    'data': [
+
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+    ],
+}
---
+++ purchase_extended/models/purchase_order.py
@@ -0,0 +1,4 @@
+from odoo import models, fields, api
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
