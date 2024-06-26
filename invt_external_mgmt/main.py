d9759fa537551389586c7c4779c68c688b08f393
riccino <riccardorinojosam@gmail.com>
2024-06-26 09:26:26 -0300

    Refactor and enhance inventory management Telegram bot - Rename connection.py to odoo_connection.py. - Create Connection class in odoo_connection.py to manage Odoo API connection:   * start: Initializes connection to Odoo using xmlrpc.client for authentication.   * get_products: Retrieves all product.template records as a list of dictionaries.   * list_products: Calls get_products to fetch records and lists them in a string with name, ID, and price, returning it to the bot.   * product_details: Returns detailed information for a specific product including name, ID, price, cost, product type, and quantity. - Add bot commands:   * products: Displays all products by executing list_products method of Connection class.   * product: Shows information for a specific product by executing product_details method of Connection class.

--- invt_external_mgmt/main.py
+++ invt_external_mgmt/main.py
@@ -1,12 +1,30 @@
 import telebot
 from dotenv import load_dotenv
 import os
+from odoo_connection import Connection
 
 load_dotenv()
+
+def set_connection():
+    connection = Connection()
+    connection.start()
+    return connection
 
 bot = telebot.TeleBot(os.environ.get("TELEGRAM_API_TOKEN"))
 @bot.message_handler(commands=["start"])
 def start(msg):
-    bot.reply_to(msg, f"Hola")
-    
+    bot.reply_to(msg, "Hi")
+
+@bot.message_handler(commands=["products"])
+def products(msg):
+    connection = set_connection()
+    bot.reply_to(msg, f"{connection.list_products()}")
+
+@bot.message_handler(commands=["product"])
+def product(msg):
+    connection = set_connection()
+    product_id = msg.text.replace("/product ", "")
+    bot.reply_to(msg, f"{connection.product_details(product_id)}")
+
+
 bot.polling(timeout=120) 
