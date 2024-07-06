63d1615a7e9543a920eb72b77eb7d65e30285f5c
riccino <riccardorinojosam@gmail.com>
2024-07-06 20:24:37 -0300

    Change comparison from product name to product id in SaleOrderLine model

--- sale_extended/models/sale_order_line.py
+++ sale_extended/models/sale_order_line.py
@@ -9,6 +9,6 @@ class SaleOrderLine(models.Model):
         # Loop through the product discounts associated with the client's type
         for product in self.order_id.partner_id.client_type_id.product_discounted_ids:
             # Check if the product id matches the selected product in the sale order line
-            if product.product_id.name == self.product_id.name:
+            if product.product_id.id == self.product_id.id:
                 # Assign the discount amount to the discount field
                 self.discount = product.to_discount
